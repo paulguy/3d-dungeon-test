558719ca5e09334228ecd360e203607f371ff99d
Paul Guy <paulguy119@gmail.com>
2025-04-17 21:50:25 -0400

    Made updating the multimesh a bit faster.
    Down from ~3300us to ~1700-2200us on my PC.

--- multi_mesh_instance_3d.gd
+++ multi_mesh_instance_3d.gd
@@ -1,12 +1,7 @@
 extends MultiMeshInstance3D
 
-var grid_depth : int
-var grid_width : int
-
-func set_view_dimensions(width : int, depth : int):
-	grid_width = width
-	grid_depth = depth
-	multimesh.instance_count = grid_width * grid_depth * 2
+func set_view_positions(positions : int):
+	multimesh.instance_count = positions * 2
 
 func refresh(floor_height : Array[float],
 			 floor_top_hue : Array[float],
@@ -26,7 +21,7 @@ func refresh(floor_height : Array[float],
 			 ceiling_face_bias : Array[float],
 			 ceiling_side_texture_offset : Array[float],
 			 ceiling_face_texture_offset : Array[float],
-			 dims : Vector2i,
+			 positions : Array[Vector2i],
 			 pos : Vector2i):
 	# color.r - top hue
 	# color.g - top bias
@@ -37,30 +32,14 @@ func refresh(floor_height : Array[float],
 	# custom.b - side texture.v
 	# custom.a - face texture.v
 
-	# TODO: Try to prevent adding tiles which will never be in view
 	var time : int = Time.get_ticks_usec()
-	var half_width : int = grid_width / 2
 	var half_array : int = multimesh.instance_count / 2
-	var i : int
-	for y in grid_depth:
-		if pos.y + y < 0 or pos.y + y >= dims.y:
-			for x in grid_width:
-				i = y * grid_width + x
-				multimesh.set_instance_transform(i, Transform3D(Basis(), Vector3(x - half_width, -1000, y)))
-				multimesh.set_instance_transform(half_array + i, Transform3D(Basis(), Vector3(x - half_width, 1000, y)))
-		else:
-			for x in grid_width:
-				i = y * grid_width + x
-				if pos.x + x < 0 or pos.x + x >= dims.x:
-					multimesh.set_instance_transform(i, Transform3D(Basis(), Vector3(x - half_width, -1000, y)))
-					multimesh.set_instance_transform(half_array + i, Transform3D(Basis(), Vector3(x - half_width, 1000, y)))
-				else:
-					var ai : int = ((pos.y + y) * dims.x) + (pos.x + x)
-					multimesh.set_instance_transform(i, Transform3D(Basis(), Vector3(0.0, floor_height[ai], 0.0)))
-					multimesh.set_instance_color(i, Color(floor_top_hue[ai], floor_top_bias[ai], floor_bottom_hue[ai], floor_bottom_bias[ai]))
-					multimesh.set_instance_custom_data(i, Color(floor_face_hue[ai], floor_face_bias[ai], floor_side_texture_offset[ai], floor_face_texture_offset[ai]))
-					multimesh.set_instance_transform(half_array + i, Transform3D(Basis(), Vector3(0.0, ceiling_height[ai], 0.0)))
-					multimesh.set_instance_color(half_array + i, Color(ceiling_top_hue[ai], ceiling_top_bias[ai], ceiling_bottom_hue[ai], ceiling_bottom_bias[ai]))
-					multimesh.set_instance_custom_data(half_array + i, Color(ceiling_face_hue[ai], ceiling_face_bias[ai], ceiling_side_texture_offset[ai], ceiling_face_texture_offset[ai]))
+	for i in half_array:
+		multimesh.set_instance_transform(i, Transform3D(Basis(), Vector3(positions[i].x, floor_height[i], positions[i].y)))
+		multimesh.set_instance_color(i, Color(floor_top_hue[i], floor_top_bias[i], floor_bottom_hue[i], floor_bottom_bias[i]))
+		multimesh.set_instance_custom_data(i, Color(floor_face_hue[i], floor_face_bias[i], floor_side_texture_offset[i], floor_face_texture_offset[i]))
+		multimesh.set_instance_transform(half_array + i, Transform3D(Basis(), Vector3(positions[i].x, ceiling_height[i], positions[i].y)))
+		multimesh.set_instance_color(half_array + i, Color(ceiling_top_hue[i], ceiling_top_bias[i], ceiling_bottom_hue[i], ceiling_bottom_bias[i]))
+		multimesh.set_instance_custom_data(half_array + i, Color(ceiling_face_hue[i], ceiling_face_bias[i], ceiling_side_texture_offset[i], ceiling_face_texture_offset[i]))
 	time = Time.get_ticks_usec() - time
 	print("%d us Refresh time " % time)
--- node_3d.gd
+++ node_3d.gd
@@ -20,32 +20,45 @@ var ceiling_face_hue : Array[float] = Array([], TYPE_FLOAT, "", null)
 var ceiling_face_bias : Array[float] = Array([], TYPE_FLOAT, "", null)
 var ceiling_side_texture_offset : Array[float] = Array([], TYPE_FLOAT, "", null)
 var ceiling_face_texture_offset : Array[float] = Array([], TYPE_FLOAT, "", null)
-var dims : Vector2i = Vector2i.ZERO
 var pos : Vector2i = Vector2i.ZERO
 
 func _ready():
-	var width : float = atan($'Camera3D'.fov / 180.0 * PI) * TEST_DEPTH * 2.0
-	$'Pillar'.mesh.material.set_shader_parameter(&"grid_width", width)
-	$'Pillar'.mesh.material.set_shader_parameter(&"grid_depth", TEST_DEPTH)
-	floor_height.resize(width * TEST_DEPTH)
-	floor_top_hue.resize(width * TEST_DEPTH)
-	floor_top_bias.resize(width * TEST_DEPTH)
-	floor_bottom_hue.resize(width * TEST_DEPTH)
-	floor_bottom_bias.resize(width * TEST_DEPTH)
-	floor_face_hue.resize(width * TEST_DEPTH)
-	floor_face_bias.resize(width * TEST_DEPTH)
-	floor_side_texture_offset.resize(width * TEST_DEPTH)
-	floor_face_texture_offset.resize(width * TEST_DEPTH)
-	ceiling_height.resize(width * TEST_DEPTH)
-	ceiling_top_hue.resize(width * TEST_DEPTH)
-	ceiling_top_bias.resize(width * TEST_DEPTH)
-	ceiling_bottom_hue.resize(width * TEST_DEPTH)
-	ceiling_bottom_bias.resize(width * TEST_DEPTH)
-	ceiling_face_hue.resize(width * TEST_DEPTH)
-	ceiling_face_bias.resize(width * TEST_DEPTH)
-	ceiling_side_texture_offset.resize(width * TEST_DEPTH)
-	ceiling_face_texture_offset.resize(width * TEST_DEPTH)
-	dims = Vector2i(width, TEST_DEPTH)
+	var fov = $'Camera3D'.fov
+	var positions : Array[Vector2i] = Array([], TYPE_VECTOR2I, "", null)
+	var count : int = 0
+	for i in TEST_DEPTH:
+		var width = ((int(ceil(atan(fov / 180 * PI) * (i + 1))) - 1) * 2) + 1
+		count += width
+		for j in width:
+			positions.append(Vector2i(j - (width / 2), i))
+	# get the smallest power of 2 it'll fit
+	# probably not needed but it doesn't hurt
+	var image_w : int = int(round(pow(2, ceil(log(count) / log(2)))))
+	var pos_image : Image = Image.create_empty(image_w, 1, false, Image.FORMAT_RGBAF)
+	for i in len(positions):
+		pos_image.set_pixel(i, 0, Color(positions[i].x, positions[i].y, 0.0, 1.0))
+	var pos_tex : ImageTexture = ImageTexture.create_from_image(pos_image)
+	$'Pillar'.mesh.material.set_shader_parameter(&'positions', pos_tex)
+	$'Pillar'.mesh.material.set_shader_parameter(&'depth', TEST_DEPTH)
+	$'Pillar'.mesh.material.set_shader_parameter(&'count', count)
+	floor_height.resize(count)
+	floor_top_hue.resize(count)
+	floor_top_bias.resize(count)
+	floor_bottom_hue.resize(count)
+	floor_bottom_bias.resize(count)
+	floor_face_hue.resize(count)
+	floor_face_bias.resize(count)
+	floor_side_texture_offset.resize(count)
+	floor_face_texture_offset.resize(count)
+	ceiling_height.resize(count)
+	ceiling_top_hue.resize(count)
+	ceiling_top_bias.resize(count)
+	ceiling_bottom_hue.resize(count)
+	ceiling_bottom_bias.resize(count)
+	ceiling_face_hue.resize(count)
+	ceiling_face_bias.resize(count)
+	ceiling_side_texture_offset.resize(count)
+	ceiling_face_texture_offset.resize(count)
 	pos = Vector2i(0, 0)
 	floor_height.fill(-1.5)
 	floor_top_bias.fill(1.0)
@@ -59,7 +72,7 @@ func _ready():
 	ceiling_face_hue.fill(0.6)
 	ceiling_height.fill(1.5)
 
-	$'MultiMeshInstance3D'.set_view_dimensions(width, TEST_DEPTH)
+	$'MultiMeshInstance3D'.set_view_positions(len(positions))
 	$'MultiMeshInstance3D'.refresh(
 		floor_height,
 		floor_top_hue,
@@ -79,4 +92,4 @@ func _ready():
 		ceiling_face_bias,
 		ceiling_side_texture_offset,
 		ceiling_face_texture_offset,
-		dims, pos)
+		positions, pos)
